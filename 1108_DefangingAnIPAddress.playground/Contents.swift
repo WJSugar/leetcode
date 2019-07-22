d202e57fa0549ffb4cb962363cf688cb947fe74b
Sugar <471043396@qq.com>
2019-07-22 18:03:35 +0800

    IP 地址无效化 --- Defanging an IP Address

---
+++ 1108_DefangingAnIPAddress.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+/*
+ 给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+ 
+ 所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+ 
+   
+ 
+ 示例 1：
+ 
+ 输入：address = "1.1.1.1"
+ 输出："1[.]1[.]1[.]1"
+ 示例 2：
+ 
+ 输入：address = "255.100.50.0"
+ 输出："255[.]100[.]50[.]0"
+   
+ 
+ 提示：
+ 
+ 给出的 address 是一个有效的 IPv4 地址
+ 
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/defanging-an-ip-address
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func defangIPaddr(_ address: String) -> String {
+    let string = address;
+    return string.replacingOccurrences(of: ".", with: "[.]")
+}
+
+
+//better solution
+func defangIPaddr_better(_ address: String) -> String {
+    var ret = ""
+    for c in address {
+        if c != Character(".") {
+            ret.append(c)
+        } else {
+            ret += "[.]"
+        }
+    }
+    return ret
+}
+
